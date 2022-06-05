b0db99e92bc71a6ef321a46b6353065e540673de
Boris Kirillov <kirillov_boris_92@mail.ru>
2022-06-05 23:06:54 +0300

    вторая проверка ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -77,12 +77,14 @@ def post_create(request):
 def post_edit(request, post_id):
     editable_post = get_object_or_404(Post, pk=post_id)
     if request.user != editable_post.author:
-        return HttpResponseRedirect(reverse('posts:post_detail', args=[post_id]))
+        return HttpResponseRedirect(
+            reverse('posts:post_detail', args=[post_id]))
     is_edit = True
     form = PostForm(request.POST, instance=editable_post)
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect(reverse('posts:post_detail', args=[post_id]))
+        return HttpResponseRedirect(
+            reverse('posts:post_detail', args=[post_id]))
     form = PostForm(instance=editable_post)
     contex = {
         'form': form,
